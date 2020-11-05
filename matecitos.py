35e161afb6348d847cdc4041bd38689ea7051685
franalbani <franalbani@users.noreply.github.com>
2020-11-05 03:50:44 -0300

    adds generator of all board configurations up to certain depth

--- matecitos.py
+++ matecitos.py
@@ -2,13 +2,15 @@
 
 import chess
 
-
 def matecitos(depth=4):
     b = chess.Board()
     matecitos = []
+    n = 0
 
     def _aux(b, level=0):
+        nonlocal n
         if level == depth:
+            n += 1
             if b.is_checkmate():
                 print(b)
                 matecitos.append(b.copy())
@@ -18,4 +20,27 @@ def matecitos(depth=4):
                 _aux(b, level=level+1)
                 b.pop()
     _aux(b)
-    return matecitos
+    return matecitos, n
+
+
+def forest(board, depth=2, copy=False):
+    '''
+    Generator over all board configurations.
+    '''
+    for move in board.legal_moves:
+        board.push(move)
+        if depth == 1:
+            yield board.copy() if copy else board
+        else:
+            yield from forest(board, depth=depth-1, copy=copy)
+        board.pop()
+
+#   def forest2(board, depth=2):
+#       if depth == 0:
+#           yield board.copy()
+#       else:
+#           for move in board.legal_moves:
+#               board.push(move)
+#               yield from forest2(board, depth=depth-1)
+#               board.pop
+#   # AssertionError: push() expects move to be pseudo-legal
