c9a7f6fbeac6290a72c58382d083225edf6c670a
franalbani <franalbani@users.noreply.github.com>
2020-11-09 12:36:12 -0300

    improves context manager

--- openings.py
+++ openings.py
@@ -6,6 +6,7 @@ from hashlib import md5
 from pathlib import Path
 import networkx as nx
 from networkx.drawing.nx_agraph import graphviz_layout, to_agraph
+from contextlib import contextmanager
 
 
 CACHE = Path('.png_cache/')
@@ -16,81 +17,81 @@ class BoardGraph:
     def __init__(self):
         self.board = chess.Board()
         self.dg = nx.DiGraph()
-        self._add_node_from_position('Initial')
+
+        node_id = self.position_id()
+        self.dg.add_node(node_id, label='Initial')
+        self.save_png()
 
     def save_svg(self, path):
         A = to_agraph(self.dg)
         A.layout('dot')
         A.draw(path)
 
+    def save_png(self):
+        node_id = self.position_id()
+        node = self.dg.nodes[node_id]
+        png_path = CACHE.joinpath(node_id + '.png')
+        svg = board2svg(self.board, arrows=node.get('arrows', []))
+        svg2png(bytestring=svg, write_to=str(png_path))
+        node['image'] = str(png_path)
+        node['shape'] = 'square'
+        node['imagepos'] = 'c'
+        node['labelloc'] = 't'
+        node['height'] = 6
+        
     def position_id(self):
         return md5(repr(self.board).encode('utf8')).hexdigest()
 
-    def _add_node_from_position(self, name, arrow=None):
-        node_id = self.position_id()
-        png = CACHE.joinpath(node_id + '.png')
-        self.save_png(arrow)
-        self.dg.add_node(node_id, label=name, image=str(png), shape='square', imagepos='c', labelloc='t', height='6')
-        return node_id
-
-    def save_png(self, arrow=None):
-        svg = board2svg(self.board, arrows=[arrow] if arrow else [])
-        node_id = self.position_id()
-        png = CACHE.joinpath(node_id + '.png')
-        svg2png(bytestring=svg, write_to=str(png))
-
-    def push(self, move, name):
-        san_move = bg.board.parse_san(move)
-        assert san_move in self.board.legal_moves
-        father_id = self.position_id()
-        self.board.push_san(move)
-        node_id = self._add_node_from_position(name, arrow=chess.svg.Arrow(san_move.from_square, san_move.to_square))
-        self.dg.add_edge(father_id, node_id, label=' ' + move)
-
-    def pop(self):
-        return self.board.pop()
-
+    @contextmanager
+    def pushed_to(self, move_san, label=''):
+        try:
+            current_id = self.position_id()
+            move = self.board.parse_san(move_san)
+            self.board.push_san(move_san)
+            new_id = self.position_id()
+            self.dg.add_node(new_id, label=label, arrows=[(move.from_square, move.to_square)])
+            self.dg.add_edge(current_id, new_id, label=' ' + move_san)
+            yield self.dg.nodes[new_id]
+            arrows = self.dg.nodes[new_id]['arrows']
+            self.save_png()
+        finally:
+            self.board.pop()
  
-from contextlib import contextmanager
-
-@contextmanager
-def line(bg, move, name):
-    try:
-        yield bg.push(move, name)
-    finally:
-        bg.pop()
-
 
 bg = BoardGraph()
 
-with line(bg, 'e4', "King's Pawn"):
-    with line(bg, 'e5', 'Open game'):
-        with line(bg, 'Nf3', "King's Knight"):
-            with line(bg, 'Nf6', "Petrov's Defense"):
-                with line(bg, 'Nxe5', '...'):
-                    with line(bg, 'Nxe4', 'Russian game: ¡¡Cuidado!!!'):
+with bg.pushed_to('e4', "King's Pawn") as position:
+    position['comment'] = 'Whites takes control of the center'
+    position['arrows'].append(chess.svg.Arrow(chess.E4, chess.D5, color='red'))
+    position['arrows'].append(chess.svg.Arrow(chess.E4, chess.F5, color='red'))
+
+    with bg.pushed_to('e5', 'Open game') as position:
+        with bg.pushed_to('Nf3', "King's Knight") as position:
+            with bg.pushed_to('Nf6', "Petrov's Defense"):
+                with bg.pushed_to('Nxe5', '...'):
+                    with bg.pushed_to('Nxe4', 'Russian game: ¡¡Cuidado!!!'):
                         pass
-            with line(bg, 'Nc6', "King's Knight: Normal variation"):
-                with line(bg, 'Bc4', 'Italian game'):
-                    with line(bg, 'Bc5', 'Italian game: Giuoco Piano'):
-                        with line(bg, 'b4', "Italian game: Evan's Gambit"):
+            with bg.pushed_to('Nc6', "King's Knight: Normal variation"):
+                with bg.pushed_to('Bc4', 'Italian game'):
+                    with bg.pushed_to('Bc5', 'Italian game: Giuoco Piano'):
+                        with bg.pushed_to('b4', "Italian game: Evan's Gambit"):
                             pass
-                    with line(bg, 'Nf6', 'Italian game: Two Knights Defense'):
-                        with line(bg, 'Ng5', 'Italian game: Two Knights Defense & Knight Attack'):
-                            with line(bg, 'd5', 'Italian game: Two Knights Defense & Knight Attack: Normal variation'):
-                                with line(bg, 'exd5', '...'):
-                                    with line(bg, 'Nxd5', '...'):
-                                        with line(bg, 'Nxf7', 'Italian game: Fried Liver attack'):
+                    with bg.pushed_to('Nf6', 'Italian game: Two Knights Defense'):
+                        with bg.pushed_to('Ng5', 'Italian game: Two Knights Defense & Knight Attack'):
+                            with bg.pushed_to('d5', 'Italian game: Two Knights Defense & Knight Attack: Normal variation'):
+                                with bg.pushed_to('exd5', '...'):
+                                    with bg.pushed_to('Nxd5', '...'):
+                                        with bg.pushed_to('Nxf7', 'Italian game: Fried Liver attack'):
                                             pass
-                with line(bg, 'Bb5', 'Ruy Lopez'):
+                with bg.pushed_to('Bb5', 'Ruy Lopez'):
                     pass
-                with line(bg, 'd4', 'Scotch Game'):
+                with bg.pushed_to('d4', 'Scotch Game'):
                     pass
-            with line(bg, 'd6', "Philidor's Defense"):
+            with bg.pushed_to('d6', "Philidor's Defense"):
                 pass
 
-with line(bg, 'd4', "Queen's Pawn"):
-    with line(bg, 'd5', "Closed game"):
-        with line(bg, 'c4', "Queen's Gambit"):
-            with line(bg, 'dxc4', "Queen's Gambit accepted"):
+with bg.pushed_to('d4', "Queen's Pawn"):
+    with bg.pushed_to('d5', "Closed game"):
+        with bg.pushed_to('c4', "Queen's Gambit"):
+            with bg.pushed_to('dxc4', "Queen's Gambit accepted"):
                 pass
